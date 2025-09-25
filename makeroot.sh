5a786c4dc320e0561107ee0a1055d2b05b8da1ff
Julio C. Rocha <dev@rocha.red>
2025-09-25 04:29:51 -0700

    Prototype OS version check

--- makeroot.sh
+++ makeroot.sh
@@ -2,43 +2,54 @@
 
 # RUN AS SUPER USER
 
+# Use Homebrew's rsync (3.x) in Monterey to prevent issues with /var/db
 rsync='/usr/local/bin/rsync'
 options='-a -h -R --progress'
-target='/Volumes/ram/target'
+jail_dir=$1
 
-# devices
-${rsync} ${options} /dev/null     ${target}/
-${rsync} ${options} /dev/ptmx     ${target}/
-${rsync} ${options} /dev/random   ${target}/
-${rsync} ${options} /dev/urandom  ${target}/
-${rsync} ${options} /dev/zero     ${target}/
+# # devices
+# ${rsync} ${options} /dev/null     ${jail_dir}/
+# ${rsync} ${options} /dev/ptmx     ${jail_dir}/
+# ${rsync} ${options} /dev/random   ${jail_dir}/
+# ${rsync} ${options} /dev/urandom  ${jail_dir}/
+# ${rsync} ${options} /dev/zero     ${jail_dir}/
 
-# Monterey+ dyld
-${rsync} ${options} /System/Library/dyld/ ${target}/
+# # Monterey+ dyld
+# ${rsync} ${options} /System/Library/dyld/ ${jail_dir}/
 
-# Ventura+ dyld
-#/System/Volumes/Preboot/Cryptexes/OS/System/Library/dyld/dyld_shared_cache_* /System/Library/dyld/
+# # Ventura+ dyld
+# #/System/Volumes/Preboot/Cryptexes/OS/System/Library/dyld/dyld_shared_cache_* /System/Library/dyld/
 
-# binaries
-${rsync} ${options} /bin/                                   ${target}/
-${rsync} ${options} /Library/Developer/CommandLineTools/    ${target}/
-${rsync} ${options} /sbin/                                  ${target}/
-${rsync} ${options} /System/Library/Frameworks/             ${target}/
-${rsync} ${options} /System/Library/Perl/                   ${target}/
-${rsync} ${options} /usr/bin/                               ${target}/
-${rsync} ${options} /usr/lib/                               ${target}/
-${rsync} ${options} /usr/libexec/                           ${target}/
-${rsync} ${options} /usr/sbin/                              ${target}/
+# # binaries
+# ${rsync} ${options} /bin/                                   ${jail_dir}/
+# ${rsync} ${options} /Library/Developer/CommandLineTools/    ${jail_dir}/
+# ${rsync} ${options} /sbin/                                  ${jail_dir}/
+# ${rsync} ${options} /System/Library/Frameworks/             ${jail_dir}/
+# ${rsync} ${options} /System/Library/Perl/                   ${jail_dir}/
+# ${rsync} ${options} /usr/bin/                               ${jail_dir}/
+# ${rsync} ${options} /usr/lib/                               ${jail_dir}/
+# ${rsync} ${options} /usr/libexec/                           ${jail_dir}/
+# ${rsync} ${options} /usr/sbin/                              ${jail_dir}/
 
-# configs
-${rsync} ${options} /etc/pam.d/                                               ${target}/
-${rsync} ${options} /etc/ssl/                                                 ${target}/
-${rsync} ${options} /etc/sudoers                                              ${target}/
-${rsync} ${options} /System/Library/CoreServices/SystemVersion.plist          ${target}/
-${rsync} ${options} /System/Library/CoreServices/SystemVersionCompat.plist    ${target}/
-${rsync} ${options} /usr/share/                                               ${target}/
-${rsync} ${options} /var/db/timezone/                                         ${target}/
+# # configs
+# ${rsync} ${options} /etc/pam.d/                                               ${jail_dir}/
+# ${rsync} ${options} /etc/ssl/                                                 ${jail_dir}/
+# ${rsync} ${options} /etc/sudoers                                              ${jail_dir}/
+# ${rsync} ${options} /System/Library/CoreServices/SystemVersion.plist          ${jail_dir}/
+# ${rsync} ${options} /System/Library/CoreServices/SystemVersionCompat.plist    ${jail_dir}/
+# ${rsync} ${options} /usr/share/                                               ${jail_dir}/
+# ${rsync} ${options} /var/db/timezone/                                         ${jail_dir}/
 
-# Chroot sentinel file
-mkdir -p ${target}/AppleInternal/XBS
-touch ${target}/AppleInternal/XBS/.isChrooted
+# # Chroot sentinel file
+# mkdir -p ${jail_dir}/AppleInternal/XBS
+# touch ${jail_dir}/AppleInternal/XBS/.isChrooted
+
+os_version=$(sw_vers -productVersion)
+if [ "${os_version%%.*}" -eq 12 ]; then
+    echo "monterey"
+elif [ "${os_version%%.*}" -ge 13 ]; then
+    echo "ventura and later"
+else
+    echo "unsupported os version"
+    exit 1
+fi
